0bb50c8965e24c29fb883c2691238a9c0bd15227
Stephen Gray <stephen_gray1255@yahoo.com>
2022-02-02 17:06:34 +1100

    my first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let bot = new RiveScript();
+
+const message_container = document.querySelector('.msg');
+const form = document.querySelector('.actions');
+const input = document.querySelector('.input');
+const username = "local-user";
+
+const brain = [
+  'https://raw.githubusercontent.com/sgray1255/rstut1/main/brain.rive'
+];
+
+bot.loadFile(brain).then(loading_done).catch(loading_error);
+
+function loading_done() {
+
+  bot.sortReplies();
+
+}
+
+function loading_error(error) {
+  console.log("Error when loading files: " + error);
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  myReply(input.value);
+  input.value = '';
+});
+
+
+function botResponse(message) {
+  message_container.innerHTML += `<div class="bot">${message}</div>`;
+  location.href = '#edge';
+}
+
+function myReply(message) {
+  message_container.innerHTML += `<div class="self">${message}</div>`;
+  location.href = '#edge';
+
+  bot.reply(username, message).then(function (reply) {
+    botResponse(reply);
+  });
+}
